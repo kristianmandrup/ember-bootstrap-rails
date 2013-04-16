11005ddf8f3e9cf45b2100d94b051ec2070071c5
Sébastien Grosjean <public@zencocoon.com>
2013-04-16 14:53:52 +0300

    Improve navs

--- app/assets/javascripts/ember-bootstrap/views/nav_list.js
+++ app/assets/javascripts/ember-bootstrap/views/nav_list.js
@@ -1,18 +1,7 @@
-var get = Ember.get, set = Ember.set;
 var Bootstrap = window.Bootstrap;
-
 Bootstrap.NavList = Ember.CollectionView.extend({
   classNames: ['nav', 'nav-list'],
   tagName: 'ul',
-
-  itemViewClass: Ember.View.extend(Bootstrap.ItemSelectionSupport, Bootstrap.ItemViewHrefSupport, {
-    template: Ember.Handlebars.compile('{{view view.item}}'),
-
-    item: Ember.View.extend({
-      tagName: 'a',
-      template: Ember.Handlebars.compile('{{view.parentView.title}}'),
-      attributeBindings: ['href'],
-      hrefBinding: 'parentView.href'
-    })
-  })
+  itemViewClass: Bootstrap.PillItem,
+  selection: null
 });
